8ed09c5e9a3e908761c33b9fd4da54a7f30256dc
luoxisteven <luoxi9932@gmail.com>
2024-10-16 01:49:37 +1100

    v0.1

--- README.md
+++ README.md
@@ -4,6 +4,27 @@
 
 This is a frontend and backend project for a student management system from https://www.bilibili.com/video/BV1U44y1W77D/.
 
+## SpringBoot Notes
+CSDN WEB: https://blog.csdn.net/cuiqwei/article/details/118188540
+
+1) Controller 连接http访问和数据 (com.example.springboot.controller;)
+2) Mapper 连接http和后台数据 (com.example.springboot.mapper;)
+3) Service 可以连接Controller和Mapper (com.example.springboot.service;)
+
+- Controller 负责处理请求和响应，连接 HTTP 请求与业务逻辑（Service）。
+- Service 负责处理业务逻辑，连接 Controller 和数据访问层（Mapper）。
+- Mapper 负责数据库操作，连接业务逻辑层（Service）和数据库。
+
+### Resources (src/main/resources)
+不同的配置文件:
+1) application.yml
+2) mapper/User.xml 配置xml之后，需要配置在application.yml
+
+
+## Vue Notes
+
+
+
 ## Run
 1)  Frontend：
     - 先 `cd springboot cd vue`
--- springboot/src/main/java/com/example/springboot/controller/UserController.java
+++ springboot/src/main/java/com/example/springboot/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springboot.entity.User;
@@ -24,20 +25,29 @@ public class UserController{
     @Autowired
     private UserService userService;
 
-    @PostMapping()
+    @PostMapping  //Post:localhost:9090/user/
     public Integer save(@RequestBody User user){ //RequestBody把前台的JSON对象转换成Java对象
         // 新增或者更新
         return userService.save(user);
     }
 
-    @GetMapping() //localhost:9090/user/
-	public List<User> index(){
+    @GetMapping //Get:localhost:9090/user/
+	public List<User> findAll(){
         // return userMapper.findAll();
         List<User> all = userMapper.findAll();
 		return all;
 	}
 
-    @DeleteMapping("/{id}") //PathVariable
+    // 分页查询, 接口为/user/page?pageNum=1&pageSize=10
+    // @RequestParam 接收
+    @GetMapping("/page") 
+    public List<User> findPage(@RequestParam Integer pageNum, @RequestParam Integer pageSize){
+        // return userMapper.findAll();
+        List<User> all = userMapper.findAll();
+		return all;
+	}
+
+    @DeleteMapping("/{id}") //Get:localhost:9090/user/{id}
     public Integer delete(@PathVariable Integer id){ //PathVariable 就是 /{id}
         return userMapper.deleteById(id);
     }
